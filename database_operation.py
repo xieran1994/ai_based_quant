166f73dbf65b0bb3fefd71c8fc1d6ca303b95bd4
xieran1994 <74256977+xieran1994@users.noreply.github.com>
2025-08-11 22:02:01 +0800

    feat: add ruff in pre-commit

--- database_operation.py
+++ database_operation.py
@@ -10,10 +10,15 @@ def ingest_data_from_csv(csv_path:Path) -> None:
     table_name :str = csv_path.split('\\')[-1].split('.')[0]
     # 数据入库
     # 中文编码需要指定为gbk， 默认为utf-8
-    # 忽略csv文件前两行，xbx文件第一行为光子，第二行为表头
+    # 忽略csv文件前两行，xbx文件第一行为广子，第二行为表头
     mysql.load_data_local_infile(csv_path=csv_path, table_name=table_name, ignore_lines=2, decoder='gbk')
 
 
-
-test_csv_path = Path(r'test_data\stock-trading-data-2024-09-28N\bj836720.csv')
-ingest_data_from_csv(csv_path=test_csv_path)
+# 遍历文件夹中的csv文件
+if __name__ == '__main__':
+    # 获取文件夹中的所有csv文件
+    csv_files = list(Path(r'test_data\stock-trading-data-2024-09-28N').glob('*.csv'))
+    # 遍历csv文件
+    for csv_file in csv_files:
+        # 数据入库
+        ingest_data_from_csv(csv_path=csv_file)
---
+++ miniquant/interface.py
@@ -0,0 +1,4 @@
+from xtquant import xtdata
+
+tdl = xtdata.get_instrument_detail('000001.SZ')
+print(tdl)
